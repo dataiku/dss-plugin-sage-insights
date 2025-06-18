e70aceb2953a8fbd89280684d754d8de62fd522b
smazzei <Stephen.Mazzei@dataiku.com>
2025-06-18 14:55:06 +0000

    Edited Library file "python/sage/src/dss_folder.py"

--- lib/python/sage/src/dss_folder.py
+++ lib/python/sage/src/dss_folder.py
@@ -52,10 +52,20 @@ def read_folder_input(folder_name, path, data_type):
     if data_type == "DF":
         with folder.get_download_stream(path) as reader:
             data = pd.read_csv(reader)
-            #data = data.convert_dtypes()
-            #data = data.infer_objects()
-            #data["lastModifiedOn"] = pd.to_datetime(data["lastModifiedOn"])
+            with warnings.catch_warnings():
+                warnings.simplefilter("ignore")  # Ignore all warnings within this block
+                data = function_with_warning(data)
     elif data_type == "JSON":
         with folder.get_download_stream(path) as reader:
             data = json.loads(reader.data)
     return data
+
+
+def function_with_warning(df):
+    for c in df.columns:
+        if df[c].dtype == "object":
+            temp_col = pd.to_datetime(df[c],  errors='coerce')
+            if temp_col.notna().all():
+                df[c] = temp_col
+    return df
+        
