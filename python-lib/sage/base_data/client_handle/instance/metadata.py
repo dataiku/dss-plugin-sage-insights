c3a96fd3b7f397c5986d3be57ce21ff6cc4505be
smazzei <Stephen.Mazzei@dataiku.com>
2025-07-23 17:41:23 +0000

    Edited file 'python-lib/sage/base_data/client_handle/instance/metadata.py'

--- python-lib/sage/base_data/client_handle/instance/metadata.py
+++ python-lib/sage/base_data/client_handle/instance/metadata.py
@@ -6,7 +6,7 @@ def main(client):
     instance_info = client.get_instance_info().raw
     df = pd.DataFrame(instance_info, index=[0])
     
-    for c in ["lastModifiedOn", "creationOn"]:
+    for c in ["dssStartupTimestamp"]:
         df[c] = pd.to_datetime(df[c], unit="ms", utc=True)
         df[c] = pd.to_datetime(df[c], utc=True)
         df[c] = df[c].fillna(pd.to_datetime("1970-01-01", utc=True))
