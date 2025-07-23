da9e86dfb255bd53c81f7fe9b906b8a87ee27911
smazzei <Stephen.Mazzei@dataiku.com>
2025-07-23 20:17:19 +0000

    Edited file 'python-lib/sage/base_data/client_handle/users/rolling_commit_user_counts.py'

--- python-lib/sage/base_data/client_handle/users/rolling_commit_user_counts.py
+++ python-lib/sage/base_data/client_handle/users/rolling_commit_user_counts.py
@@ -22,3 +22,16 @@ def get_remote_dataframe(client, table_name):
 def main(client):
     # Pull in DSS Commits table to see user activity better
     dss_commits_df = get_remote_dataframe(client, "dss_commits")
+    
+    # Compute the rolling unique count of users per month
+    df = dss_commits_df[["author", "timestamp"]]
+    df = df[~df["author"].str.contains("api:")]
+    df["timestamp"] = pd.to_datetime(df["timestamp"], unit="ms", utc=True)
+    df["year"] = df["timestamp"].dt.year.astype(str)
+    df["month"] = df["timestamp"].dt.month.astype(str)
+    df = df.groupby(by=["year", "month"])["author"].nunique().reset_index()
+    df["date"] = df["year"].str.cat(df["month"], sep="-")
+    df["date"] = pd.to_datetime(df["date"], format="%Y-%m")
+    
+    # Return
+    return df
