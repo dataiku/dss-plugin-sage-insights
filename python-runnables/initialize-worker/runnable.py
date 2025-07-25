610894718aae650fdf06039c5c9ee1d0f0a10b51
smazzei <Stephen.Mazzei@dataiku.com>
2025-07-25 20:07:04 +0000

    Edited files

--- python-runnables/initialize-worker/runnable.py
+++ python-runnables/initialize-worker/runnable.py
@@ -32,13 +32,12 @@ class MyRunnable(Runnable):
             
             # Install/Update Plugin if not found
             cont = True
-            if self.sage_project_url != worker_url:
-                try:
-                    dss_init.install_plugin(self, remote_client)
-                    results.append([worker_url, "Plugin Configured", True, None])
-                except Exception as e:
-                    results.append([worker_url, "Plugin Configured", False, e])
-                    cont = False
+            try:
+                dss_init.install_plugin(self, remote_client)
+                results.append([worker_url, "Plugin Configured", True, None])
+            except Exception as e:
+                results.append([worker_url, "Plugin Configured", False, e])
+                cont = False
 
             # Create the Sage Worker Project
             if cont:
