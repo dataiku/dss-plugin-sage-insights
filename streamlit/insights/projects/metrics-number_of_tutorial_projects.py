f3b67fa30aaf1b64fd5204fda4affb21ad5d1e82
admin <admin>
2025-07-25 21:26:56 +0000

    Uploaded file 'metrics-number_of_tutorial_projects.py' into '/streamlit/insights/projects'

---
+++ streamlit/insights/projects/metrics-number_of_tutorial_projects.py
@@ -0,0 +1,33 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+from sage.insights.data_structures import structures
+from sage.src import dss_funcs, dss_folder
+
+local_client = dss_funcs.build_local_client()
+project_handle = local_client.get_default_project()
+sage_project_key = project_handle.project_key
+
+def main(df=pd.DataFrame()):
+    # load data structure
+    data = structures.get("metric")
+
+    # Load additional data
+    if df.empty:
+        df = dss_folder.read_local_folder_input(
+            sage_project_key = sage_project_key,
+            project_handle = project_handle,
+            folder_name="base_data",
+            path=f"/projects/metadata.csv" # change this line
+        )
+
+    # Perform logic here
+    # count how many rows of column 'tags' contain 'tutorial'
+    count = df['tags'].apply(lambda x: 'tutorial' in x).sum()
+
+    # Set values
+    data["label"] = "Total DSS Tutorial Projects"
+    data["data"] = count
+
+    return data
