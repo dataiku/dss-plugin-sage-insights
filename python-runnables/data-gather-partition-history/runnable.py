e8a03f98ab811e0643d130ab55df4872c473b19d
smazzei <Stephen.Mazzei@dataiku.com>
2025-07-29 21:11:54 +0000

    Edited file 'python-runnables/data-gather-partition-history/runnable.py'

--- python-runnables/data-gather-partition-history/runnable.py
+++ python-runnables/data-gather-partition-history/runnable.py
@@ -17,6 +17,10 @@ class MyRunnable(Runnable):
         self.sage_project_api = plugin_config.get("sage_project_api", None)
         self.dt = datetime.utcnow()
         
+        # Set environment variable
+        self.sage_folder_connection = plugin_config.get("sage_folder_connection", "filesystem_folders")
+        os.environ["SAGE_FOLDER_CONNECTION"] = self.sage_folder_connection
+        
     def get_progress_target(self):
         return None
 
