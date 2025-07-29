be22e3821ce1c809a6688131cb6842b149c951c4
smazzei <Stephen.Mazzei@dataiku.com>
2025-07-29 18:56:56 +0000

    Edited file 'python-lib/sage/base_data/client_handle/users/git_history.py'

--- python-lib/sage/base_data/client_handle/users/git_history.py
+++ python-lib/sage/base_data/client_handle/users/git_history.py
@@ -5,6 +5,10 @@ from joblib import Parallel, delayed
 from datetime import datetime, date, timedelta
 
 
+today = date.today()
+yesterday = today - timedelta(days=1)
+
+
 def split_work(client, project_keys):
     df = pd.DataFrame()
     for project_key in project_keys:
@@ -29,8 +33,6 @@ def split_work(client, project_keys):
 def main(client):
     project_keys = client.list_project_keys()
     pkey_array = np.array_split(project_keys, 4)
-    today = date.today()
-    yesterday = today - timedelta(days=1)
     results = Parallel(n_jobs=4)(delayed(split_work)(client=client, project_keys=i) for i in pkey_array)
     df = pd.concat(results, ignore_index=True)
     return df
