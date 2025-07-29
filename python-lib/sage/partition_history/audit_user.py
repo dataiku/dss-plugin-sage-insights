7c9d81ef173daeaf914a88a4aab6584a0215c5d0
smazzei <Stephen.Mazzei@dataiku.com>
2025-07-29 20:31:27 +0000

    Edited file 'python-lib/sage/partition_history/audit_user.py'

--- python-lib/sage/partition_history/audit_user.py
+++ python-lib/sage/partition_history/audit_user.py
@@ -2,7 +2,7 @@ from sage.src import dss_funcs, dss_folder
 
 import pandas as pd
 
-def main(self, project_handle, df):
+def main(self, project_handle, folder, df):
     # Select the partition
     partitions = df[
         (df["category"] == "users")
