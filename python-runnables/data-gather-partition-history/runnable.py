b694e0c64e8de482cbcf81a3e658f39e86cac476
smazzei <Stephen.Mazzei@dataiku.com>
2025-07-29 20:21:19 +0000

    Edited file 'python-runnables/data-gather-partition-history/runnable.py'

--- python-runnables/data-gather-partition-history/runnable.py
+++ python-runnables/data-gather-partition-history/runnable.py
@@ -1,30 +1,84 @@
-# This file is the actual code for the Python runnable data-gather-partition-history
+from sage.src import dss_funcs, dss_folder
+
+import pandas as pd
+from datetime import datetime
+
 from dataiku.runnables import Runnable
 
 class MyRunnable(Runnable):
-    """The base interface for a Python runnable"""
-
     def __init__(self, project_key, config, plugin_config):
-        """
-        :param project_key: the project in which the runnable executes
-        :param config: the dict of the configuration of the object
-        :param plugin_config: contains the plugin settings
-        """
         self.project_key = project_key
         self.config = config
         self.plugin_config = plugin_config
+        self.sage_project_key = plugin_config.get("sage_project_key", None)
+        self.sage_project_url = plugin_config.get("sage_project_url", None)
+        self.sage_project_api = plugin_config.get("sage_project_api", None)
+        self.dt = datetime.utcnow()
         
     def get_progress_target(self):
-        """
-        If the runnable will return some progress info, have this function return a tuple of 
-        (target, unit) where unit is one of: SIZE, FILES, RECORDS, NONE
-        """
         return None
 
     def run(self, progress_callback):
-        """
-        Do stuff here. Can return a string or raise an exception.
-        The progress_callback is a function expecting 1 value: current progress
-        """
-        raise Exception("unimplemented")
+        results = []
+        # get partitioned folder
+        local_client = dss_funcs.build_local_client()
+        project_handle = local_client.get_project(project_key=self.sage_project_key)
+        folder = dss_folder.get_folder(self.sage_project_key, project_handle, "partitioned_data")
         
+        # list partitions and turn into a df
+        partitions = folder.list_partitions()
+        folder_df = pd.DataFrame(partitions, columns=["partitions"])
+        cols = ["instance_name", "category", "module", "dt"]
+        folder_df[cols] = folder_df["partitions"].str.split("|", expand=True)
+        results.append(["List Partitions", True, None])
+        
+        # get latest partition
+        max_date = folder_df['dt'].max()
+        dss_folder.write_local_folder_output(
+            sage_project_key = self.sage_project_key,
+            project_handle = project_handle,
+            folder_name = "base_data",
+            path = f"/partition.csv",
+            data_type = "DF",
+            data = pd.DataFrame([max_date], columns=["latest_partition"])
+        )
+        filtered_df = folder_df[folder_df['dt'] == max_date]
+        results.append(["Newest Partition", True, None])
+        
+        # Loop over the sets and gather
+        groups = filtered_df.groupby(by=["category", "module"])
+        for i, g in groups:
+            category, module = i
+            # loop over and build consolidated df
+            df = pd.DataFrame()
+            for partition in g["partitions"].tolist():
+                paths = folder.list_paths_in_partition(partition=partition)
+                for path in paths:
+                    tdf = dss_folder.read_local_folder_input(
+                        sage_project_key = self.sage_project_key,
+                        project_handle = project_handle,
+                        folder_name = "partitioned_data",
+                        path = path
+                    )
+                    if df.empty:
+                        df = tdf
+                    else:
+                        df = pd.concat([df, tdf], ignore_index=True)
+                # Write consolidated DF to folder
+                dss_folder.write_local_folder_output(
+                    sage_project_key = self.sage_project_key,
+                    project_handle = project_handle,
+                    folder_name = "base_data",
+                    path = f"/{category}/{module}.csv",
+                    data_type = "DF",
+                    data = df
+                )
+        results.append(["Stack newest datasets", True, None])
+                
+        
+        # return results
+        if results:
+            df = pd.DataFrame(results, columns=["step", "result", "message"])
+            html = df.to_html()
+            return html
+        raise Exception("FAILED TO RUN PROJECT CHECKS")
