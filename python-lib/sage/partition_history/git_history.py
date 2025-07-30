83d0e77fd54b34982a6ce03238be8ed8be1b570a
smazzei <Stephen.Mazzei@dataiku.com>
2025-07-30 16:31:02 +0000

    Edited file 'python-lib/sage/partition_history/git_history.py'

--- python-lib/sage/partition_history/git_history.py
+++ python-lib/sage/partition_history/git_history.py
@@ -10,46 +10,27 @@ def main(self, project_handle, folder, df):
     ]["partition"].tolist()
     
     # Load the df
-    login_users_df = pd.DataFrame()
-    active_users_df = pd.DataFrame()
+    git_history_df = pd.DataFrame()
     for partition in partitions:
         for path in folder.get_partition_info(partition)["paths"]:
             with folder.get_download_stream(path=path) as r:
                 df = pd.read_csv(r)
-            
-            # Login Users
-            login_users = df[df["message.msgType"] == "application-open"]["message.authUser"].unique()
-            login_users_tmp_df = pd.DataFrame([len(login_users)], columns=["count"])
-            login_users_tmp_df["timestamp"] = df["timestamp"].iloc[0]
-            login_users_tmp_df["instance_name"] = df["instance_name"].iloc[0]
-            if login_users_df.empty:
-                login_users_df = login_users_tmp_df
+            # Build Git History Table
+            df["timestamp"] = pd.to_datetime(df["timestamp"])
+            git_history_tmp_df = df.groupby(["instance_name", "author"])["author"].size().reset_index(name="count")
+            git_history_tmp_df["timestamp"] = df["timestamp"].iloc[0].to_period("D")
+            if git_history_df.empty:
+                git_history_df = git_history_tmp_df
             else:
-                login_users_df = pd.concat([login_users_df, login_users_tmp_df], ignore_index=True)
+                git_history_df = pd.concat([git_history_df, git_history_tmp_df], ignore_index=True)
 
-            # Active Users
-            tdf = df[df["message.authUser"].isin(login_users)]
-            action_words = ["save", "create", "analysis", "clear", "run"] # Action Words -- Focus on
-            pattern = "|".join(action_words)
-            tdf = tdf[tdf["message.msgType"].str.contains(pattern, na=False)]
-            remove_strings = ["list", "dataset-clear-samples", "dataset-save-schema", "project-save-variables"] # Vague Words -- Remove
-            pattern = "|".join(remove_strings)
-            tdf = tdf[~tdf["message.msgType"].str.contains(pattern, na=False)]
-            active_users = tdf["message.authUser"].unique()
-            active_users_tmp_df = pd.DataFrame([len(active_users)], columns=["count"])
-            active_users_tmp_df["timestamp"] = df["timestamp"].iloc[0]
-            active_users_tmp_df["instance_name"] = df["instance_name"].iloc[0]
-            if login_users_df.empty:
-                active_users_df = active_users_tmp_df
-            else:
-                active_users_df = pd.concat([active_users_df, active_users_tmp_df], ignore_index=True)
 
     # Write consolidated DF to folder
     dss_folder.write_local_folder_output(
         sage_project_key = self.sage_project_key,
         project_handle = project_handle,
         folder_name = "base_data",
-        path = f"/users/login_users.csv",
+        path = f"/users/rolling_git_history.csv",
         data_type = "DF",
         data = login_users_df
     )
