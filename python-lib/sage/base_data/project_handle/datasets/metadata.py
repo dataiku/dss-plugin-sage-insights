5504b273e7b5182a56ca49c889443a4bb6d1a9d8
smazzei <Stephen.Mazzei@dataiku.com>
2025-07-31 19:42:27 +0000

    Edited file 'python-lib/sage/base_data/project_handle/datasets/metadata.py'

--- python-lib/sage/base_data/project_handle/datasets/metadata.py
+++ python-lib/sage/base_data/project_handle/datasets/metadata.py
@@ -5,21 +5,22 @@ from sage.src.dss_funcs import get_nested_value
 def main(project_handle):
     df = pd.DataFrame()
     for dataset in project_handle.list_datasets():
-        projectKey = dataset.get("projectKey", False)
-        dataset_name = dataset.get("name", False)
-        dataset_type = dataset.get("type", False)
-        dataset_managed = dataset.get("managed", False)
-        formatType = get_nested_value(dataset, ["formatType"])
-        lastModifiedBy = get_nested_value(dataset, ["versionTag", "lastModifiedBy", "login"])
-        lastModifiedOn = get_nested_value(dataset, ["versionTag", "lastModifiedOn"])
-        creationBy = get_nested_value(dataset, ["creationTag", "lastModifiedBy", "login"])
-        creationOn = get_nested_value(dataset, ["creationTag", "lastModifiedOn"])
-        tags = dataset.get("tags", False)
-        d = [
-            projectKey, dataset_name, dataset_type, dataset_managed, formatType,
-            lastModifiedBy, lastModifiedOn, creationBy, creationOn,
-            tags            
-        ]
-        tdf = pd.DataFrame([d], columns=cols)
-        df = pd.concat([df, tdf], ignore_index=True)
+        d = {"projectKey": project_handle.project_key}
+        ["dataset_name"] = dataset.get("name", False)
+        ["dataset_type"] = dataset.get("type", False)
+        ["dataset_managed"] = dataset.get("managed", False)
+        ["formatType"] = get_nested_value(dataset, ["formatType"])
+        ["lastModifiedBy"] = get_nested_value(dataset, ["versionTag", "lastModifiedBy", "login"])
+        ["lastModifiedOn"] = get_nested_value(dataset, ["versionTag", "lastModifiedOn"])
+        ["creationBy"] = get_nested_value(dataset, ["creationTag", "lastModifiedBy", "login"])
+        ["creationOn"] = get_nested_value(dataset, ["creationTag", "lastModifiedOn"])
+        ["tags"] = dataset.get("tags", False)
+        
+        # turn to dataframe
+        tdf = pd.DataFrame([d])
+        if df.empty:
+            df = tdf
+        else:
+            df = pd.concat([df, tdf], ignore_index=True)
+    return df
     return df
