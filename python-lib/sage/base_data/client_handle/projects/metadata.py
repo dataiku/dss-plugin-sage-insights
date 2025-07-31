f3046fbdff6ddff0a9126a086dd6e6498212cf29
smazzei <Stephen.Mazzei@dataiku.com>
2025-07-31 19:49:42 +0000

    Edited file 'python-lib/sage/base_data/client_handle/projects/metadata.py'

--- python-lib/sage/base_data/client_handle/projects/metadata.py
+++ python-lib/sage/base_data/client_handle/projects/metadata.py
@@ -12,10 +12,10 @@ def main(client):
         d["project_name"] = project.get("name", False)
         d["project_login"] = project.get("ownerLogin", False)
         d["project_login_dn"] = get_nested_value(project, ["ownerDisplayName"])
-        d["lastModifiedBy"] = get_nested_value(project, ["versionTag", "lastModifiedBy", "login"])
-        d["lastModifiedOn"] = get_nested_value(project, ["versionTag", "lastModifiedOn"])
-        d["creationBy"] = get_nested_value(project, ["creationTag", "lastModifiedBy", "login"])
-        d["creationOn"] = get_nested_value(project, ["creationTag", "lastModifiedOn"])
+        d["project_last_mod_by"] = get_nested_value(project, ["versionTag", "lastModifiedBy", "login"])
+        d["project_last_mod_dt"] = get_nested_value(project, ["versionTag", "lastModifiedOn"])
+        d["project_last_create_by"] = get_nested_value(project, ["creationTag", "lastModifiedBy", "login"])
+        d["project_last_create_dt"] = get_nested_value(project, ["creationTag", "lastModifiedOn"])
         d["shortDesc"] = project.get("shortDesc", False)
         d["tags"] = project.get("tags", False)
 
@@ -27,13 +27,12 @@ def main(client):
             df = pd.concat([df, tdf], ignore_index=True)
     
     # Imported projects missing creation values - temp fix for now
-    df.loc[df["creationBy"] == False, "creationBy"] = df["lastModifiedBy"]
-    df.loc[df["creationOn"] == 0, "creationOn"] = df["lastModifiedOn"]
+    df.loc[df["project_last_create_by"] == False, "project_last_create_by"] = df["project_last_mod_by"]
+    df.loc[df["project_last_create_dt"] == 0, "project_last_create_dt"]     = df["project_last_mod_dt"]
 
     # Clean dates
-    for c in ["lastModifiedOn", "creationOn"]:
+    for c in ["project_last_mod_dt", "project_last_create_dt"]:
         df[c] = pd.to_datetime(df[c], unit="ms", utc=True)
-        df[c] = pd.to_datetime(df[c], utc=True)
         df[c] = df[c].fillna(pd.to_datetime("1970-01-01", utc=True))
         df[c] = df[c].dt.strftime("%Y-%m-%d %H:%M:%S.%f")
 
