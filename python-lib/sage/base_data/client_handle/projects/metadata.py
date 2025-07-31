73ce191cd1655eb3a4d29febd5e9b28f45665cc3
smazzei <Stephen.Mazzei@dataiku.com>
2025-07-31 20:43:13 +0000

    Edited file 'python-lib/sage/base_data/client_handle/projects/metadata.py'

--- python-lib/sage/base_data/client_handle/projects/metadata.py
+++ python-lib/sage/base_data/client_handle/projects/metadata.py
@@ -8,7 +8,7 @@ def main(client):
         d = {}
         
         # Poll Data
-        d["projectKey"] = project.get("projectKey", False)
+        d["project_key"] = project.get("projectKey", False)
         d["project_name"] = project.get("name", False)
         d["project_login"] = project.get("ownerLogin", False)
         d["project_login_dn"] = get_nested_value(project, ["ownerDisplayName"])
