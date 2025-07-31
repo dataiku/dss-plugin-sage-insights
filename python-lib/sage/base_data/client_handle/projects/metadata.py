cf6f5c861f43cd0320a5e4b63fe4f6b0d201d831
smazzei <Stephen.Mazzei@dataiku.com>
2025-07-31 19:46:43 +0000

    Edited file 'python-lib/sage/base_data/client_handle/projects/metadata.py'

--- python-lib/sage/base_data/client_handle/projects/metadata.py
+++ python-lib/sage/base_data/client_handle/projects/metadata.py
@@ -5,22 +5,22 @@ from sage.src.dss_funcs import get_nested_value
 def main(client):
     df = pd.DataFrame()
     for project in client.list_projects():
-        projectKey = project.get("projectKey", False)
-        ownerLogin = project.get("ownerLogin", False)
-        name = project.get("name", False)
-        ownerDisplayName = get_nested_value(project, ["ownerDisplayName"])
-        lastModifiedBy = get_nested_value(project, ["versionTag", "lastModifiedBy", "login"])
-        lastModifiedOn = get_nested_value(project, ["versionTag", "lastModifiedOn"])
-        creationBy = get_nested_value(project, ["creationTag", "lastModifiedBy", "login"])
-        creationOn = get_nested_value(project, ["creationTag", "lastModifiedOn"])
-        shortDesc = project.get("shortDesc", False)
-        tags = project.get("tags", False)
-        d = [
-            projectKey, name, ownerLogin, ownerDisplayName,
-            lastModifiedBy, lastModifiedOn, creationBy, creationOn,
-            shortDesc, tags
-        ]
-        tdf = pd.DataFrame([d], columns=cols)
+        d = {}
+        
+        # Poll Data
+        d["projectKey"] = project.get("projectKey", False)
+        d["ownerLogin"] = project.get("ownerLogin", False)
+        d["name"] = project.get("name", False)
+        d["ownerDisplayName"] = get_nested_value(project, ["ownerDisplayName"])
+        d["lastModifiedBy"] = get_nested_value(project, ["versionTag", "lastModifiedBy", "login"])
+        d["lastModifiedOn"] = get_nested_value(project, ["versionTag", "lastModifiedOn"])
+        d["creationBy"] = get_nested_value(project, ["creationTag", "lastModifiedBy", "login"])
+        d["creationOn"] = get_nested_value(project, ["creationTag", "lastModifiedOn"])
+        d["shortDesc"] = project.get("shortDesc", False)
+        d["tags"] = project.get("tags", False)
+
+        # turn to dataframe
+        tdf = pd.DataFrame([d])
         if df.empty:
             df = tdf
         else:
