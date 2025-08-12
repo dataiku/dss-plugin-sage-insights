1543bd72431ff0c2f60f939ef872e8433de86980
smazzei <Stephen.Mazzei@dataiku.com>
2025-08-12 18:14:50 +0000

    Edited files

--- python-runnables/data-gather-audit-user-login/runnable.py
+++ python-runnables/data-gather-audit-user-login/runnable.py
@@ -62,10 +62,6 @@ class MyRunnable(Runnable):
         jdf = pd.json_normalize(df["message"]).add_prefix("message.").reset_index(drop=True)
         df = df.drop(columns="message").reset_index(drop=True)
         df = pd.concat([df, jdf], axis=1)
-        drop_cols = [
-            "severity", "logger", "topic", "mdc", "callTime", "timestamp"
-        ]
-        df.drop(columns=drop_cols, inplace=True)
         
         # Remove scenarios, job and NaN's
         if "message.scenarioId" in df.columns:
