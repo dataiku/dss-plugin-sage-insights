358971a68bf7c230be952f69705bbd10945e56d1
smazzei <Stephen.Mazzei@dataiku.com>
2025-08-14 18:48:52 +0000

    Edited file 'python-lib/sage/base_data/client_handle/instance/name_mapping.py'

--- python-lib/sage/base_data/client_handle/instance/name_mapping.py
+++ python-lib/sage/base_data/client_handle/instance/name_mapping.py
@@ -5,5 +5,4 @@ from sage.src.dss_funcs import get_nested_value
 def main(client):
     mapping = get_dss_name_id_mapping(client)
     df = pd.DataFrame([mapping], columns=["instance_name", "instance_name_base", "instance_id_base"])
-
     return df
