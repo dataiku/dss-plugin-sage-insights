c7ba028a96adb583a299105606ff48e81c89e52a
smazzei <Stephen.Mazzei@dataiku.com>
2025-08-14 18:48:44 +0000

    Edited file 'python-lib/sage/base_data/client_handle/instance/name_mapping.py'

--- python-lib/sage/base_data/client_handle/instance/name_mapping.py
+++ python-lib/sage/base_data/client_handle/instance/name_mapping.py
@@ -3,13 +3,7 @@ from sage.src.dss_funcs import get_nested_value
 
 
 def main(client):
-    instance_info = client.get_instance_info().raw
-    df = pd.DataFrame(instance_info, index=[0])
-    
-    for c in ["dssStartupTimestamp"]:
-        df[c] = pd.to_datetime(df[c], unit="ms", utc=True)
-        df[c] = pd.to_datetime(df[c], utc=True)
-        df[c] = df[c].fillna(pd.to_datetime("1970-01-01", utc=True))
-        df[c] = df[c].dt.strftime("%Y-%m-%d %H:%M:%S.%f")
+    mapping = get_dss_name_id_mapping(client)
+    df = pd.DataFrame([mapping], columns=["instance_name", "instance_name_base", "instance_id_base"])
 
     return df
