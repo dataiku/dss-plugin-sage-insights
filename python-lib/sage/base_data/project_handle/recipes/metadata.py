9dae9cccecd77cc763ef2ca96df72f801032800a
smazzei <Stephen.Mazzei@dataiku.com>
2025-08-23 02:14:32 +0000

    Edited file 'python-lib/sage/base_data/project_handle/recipes/metadata.py'

--- python-lib/sage/base_data/project_handle/recipes/metadata.py
+++ python-lib/sage/base_data/project_handle/recipes/metadata.py
@@ -3,7 +3,7 @@ from sage.src.dss_funcs import get_nested_value
 
 
 def main(project_handle, client_d = {}):
-    
+    # Get project level python code environment
     project_python_env = project_handle.get_settings().settings["settings"]["codeEnvs"]["python"]["mode"]
     if project_python_env == "USE_BUILTIN_MODE":
         python_env_name = "USE_BUILTIN_MODE"
@@ -28,6 +28,16 @@ def main(project_handle, client_d = {}):
         
         d["recipe_last_mod_dt"] = pd.to_datetime(d["recipe_last_mod_dt"], unit="ms")
         d["recipe_last_create_dt"] = pd.to_datetime(d["recipe_last_create_dt"], unit="ms")
+        
+        if recipe["type"] == "python":
+            recipe_handle = project_handle.get_recipe(recipe["name"])
+            d["recipe_code_env_mode"] = recipe_handle.get_settings().data["recipe"]["params"]["envSelection"]["envMode"]
+            if d["recipe_code_env_mode"] == "USE_BUILTIN_MODE":
+                python_env_name = "USE_BUILTIN_MODE"  
+            elif d["recipe_code_env_mode"] == "INHERIT":
+                d["recipe_code_env_name"] = python_env_name
+            else:
+                d["recipe_code_env_name"] = recipe_handle.get_settings().data["recipe"]["params"]["envSelection"]["envName"]
         
         # turn to dataframe
         tdf = pd.DataFrame([d])
