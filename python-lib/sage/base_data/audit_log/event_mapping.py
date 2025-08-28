f48e3ec65fac47e25dd684950f159fe1e9ed37ed
smazzei <Stephen.Mazzei@dataiku.com>
2025-08-28 20:39:34 +0000

    Edited file 'python-lib/sage/base_data/audit_log/event_mapping.py'

--- python-lib/sage/base_data/audit_log/event_mapping.py
+++ python-lib/sage/base_data/audit_log/event_mapping.py
@@ -1,5 +1,8 @@
 import pandas as pd
 from sage.src import dss_folder
+from sage.base_data.audit_log.event_tagging_drop import
+from sage.base_data.audit_log.event_tagging_mapped import
+
 
 
 def main(self, remote_client, df):
