1de6da8de07c176137543fff93393855d62c1eef
smazzei <Stephen.Mazzei@dataiku.com>
2025-08-28 15:46:40 +0000

    Edited file 'python-runnables/data-gather-audit-logs/runnable.py'

--- python-runnables/data-gather-audit-logs/runnable.py
+++ python-runnables/data-gather-audit-logs/runnable.py
@@ -81,20 +81,20 @@ class MyRunnable(Runnable):
         df["timestamp"] = pd.to_datetime(df["timestamp"])
         #df = df[df["timestamp"] >= last_update]
         return str(len(df))
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# EOF
