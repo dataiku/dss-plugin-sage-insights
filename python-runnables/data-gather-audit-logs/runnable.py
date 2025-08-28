17bb7ee8882c1130f30c6bd3a6ec2dccebc9fbed
smazzei <Stephen.Mazzei@dataiku.com>
2025-08-28 20:55:53 +0000

    Edited file 'python-runnables/data-gather-audit-logs/runnable.py'

--- python-runnables/data-gather-audit-logs/runnable.py
+++ python-runnables/data-gather-audit-logs/runnable.py
@@ -97,7 +97,7 @@ class MyRunnable(Runnable):
         #else:
         #    results.append(r)
 
-        r = user_login.main(self, remote_client, df)
+        r = event_mapping.main(self, remote_client, df)
         ###results.append(["Event Mapping", result[0], result[1]])        
         ## cru_logs(df)
         ###results.append(["CRU Logs", result[0], result[1]])
