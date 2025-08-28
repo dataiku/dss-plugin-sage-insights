5c57b9a4a791024afd1a40826aa967289e0521b1
smazzei <Stephen.Mazzei@dataiku.com>
2025-08-28 18:25:28 +0000

    Edited file 'python-lib/sage/base_data/audit_log/user_login.py'

--- python-lib/sage/base_data/audit_log/user_login.py
+++ python-lib/sage/base_data/audit_log/user_login.py
@@ -14,3 +14,23 @@ def user_login(df):
         df = df[["timestamp", "date", "message.callPath", "message.msgType", "message.authUser", "message.projectKey", "instance_name"]]
     except:
         print("RETURN")
+        
+    for i,grp in df.groupby("date"):
+        # Login Users
+        login_users = grp[grp["message.msgType"] == "application-open"]["message.authUser"].unique()
+        login_users_df = pd.DataFrame([login_users], columns=["user_logins"])
+        login_users_df["timestamp"] = pd.to_datetime(i)
+        login_users_df["instance_name"] = df["instance_name"].iloc[0]
+
+        # Active Users
+        tdf = grp[grp["message.authUser"].isin(login_users)]
+        action_words = ["save", "create", "analysis", "clear", "run"] # Action Words -- Focus on
+        pattern = "|".join(action_words)
+        tdf = tdf[tdf["message.msgType"].str.contains(pattern, na=False)]
+        remove_strings = ["list", "dataset-clear-samples", "dataset-save-schema", "project-save-variables"] # Vague Words -- Remove
+        pattern = "|".join(remove_strings)
+        tdf = tdf[~tdf["message.msgType"].str.contains(pattern, na=False)]
+        active_users = tdf["message.authUser"].unique()
+        active_users_df = pd.DataFrame([active_users], columns=["developer_user_logins"])
+        active_users_df["timestamp"] = pd.to_datetime(i)
+        active_users_df["instance_name"] = df["instance_name"].iloc[0]
