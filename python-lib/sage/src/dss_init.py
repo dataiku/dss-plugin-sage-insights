3f6f24f20c44e431b8d00bd6dd1f2bfc92b6fb45
smazzei <Stephen.Mazzei@dataiku.com>
2025-08-29 15:43:48 +0000

    Edited file 'python-lib/sage/src/dss_init.py'

--- python-lib/sage/src/dss_init.py
+++ python-lib/sage/src/dss_init.py
@@ -60,7 +60,6 @@ def install_plugin(self, remote_client):
         plugin_handle.update_from_git(repository_url=self.sage_repo_url, checkout=self.sage_repo_branch)
         update_plugin_config(self, plugin_handle)
     else:
-        # install the plugin
         plugin_install = remote_client.install_plugin_from_git(
             repository_url=self.sage_repo_url, checkout=self.sage_repo_branch, subpath=None
         )
