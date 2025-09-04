80f0aba261d55ee6cf9927f207836d30c1d2a043
smazzei <Stephen.Mazzei@dataiku.com>
2025-09-04 17:07:29 +0000

    Edited file 'python-lib/sage/base_data/project_handle/agent_tools/metadata.py'

--- python-lib/sage/base_data/project_handle/agent_tools/metadata.py
+++ python-lib/sage/base_data/project_handle/agent_tools/metadata.py
@@ -3,6 +3,7 @@ from sage.src import dss_funcs
 
 
 def main(project_handle, client_d = {}):
-    df = pd.json_normalize(project_handle.list_agent_tools())
-    df = dss_funcs.rename_and_move_first(project_handle, df, "projectKey", "project_key")
+    prefix = "agent_tools"
+    df = pd.json_normalize(project_handle.list_agent_tools()).add_prefix(f"{prefix}_")
+    df = dss_funcs.rename_and_move_first(project_handle, df, f"{prefix}_projectKey", "project_key")
     return df
