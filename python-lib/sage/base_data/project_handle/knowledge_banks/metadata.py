c5d86e2b72df99b4891f7d41cc1be0c565f1abc3
smazzei <Stephen.Mazzei@dataiku.com>
2025-09-04 15:00:06 +0000

    Edited file 'python-lib/sage/base_data/project_handle/knowledge_banks/metadata.py'

--- python-lib/sage/base_data/project_handle/knowledge_banks/metadata.py
+++ python-lib/sage/base_data/project_handle/knowledge_banks/metadata.py
@@ -3,6 +3,6 @@ from sage.src import dss_funcs
 
 
 def main(project_handle, client_d = {}):
-    df = pd.json_normalize(project_handle.list_agent_tools())
-    df = dss_funcs.rename_and_move_first(project_handle, df, "projectKey", "project_key")
+    df = pd.json_normalize(project_handle.list_knowledge_banks())
+    df = rename_and_move_first(project_handle, df, "projectKey", "project_key")
     return df
