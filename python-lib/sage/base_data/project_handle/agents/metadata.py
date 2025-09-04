211faa495e50c19943a25b532811e4b39f9a2aec
smazzei <Stephen.Mazzei@dataiku.com>
2025-09-04 17:06:50 +0000

    Edited file 'python-lib/sage/base_data/project_handle/agents/metadata.py'

--- python-lib/sage/base_data/project_handle/agents/metadata.py
+++ python-lib/sage/base_data/project_handle/agents/metadata.py
@@ -7,8 +7,8 @@ def main(project_handle, client_d = {}):
     df = pd.json_normalize(project_handle.list_agents()).add_prefix(f"{prefix}_")
     df = df.explode(f"{prefix}_versions").reset_index(drop=True)
     df = pd.concat([
-        df.drop(columns=["versions"]),
-        pd.json_normalize(df["versions"]).add_prefix("verions_")
+        df.drop(columns=[f"{prefix}_versions"]),
+        pd.json_normalize(df[f"{prefix}_versions"]).add_prefix(f"{prefix}_verions_")
     ], axis=1)
     df.columns = df.columns.str.replace('.', '_', regex=False)
     df = rename_and_move_first(project_handle, df, "projectKey", "project_key")
