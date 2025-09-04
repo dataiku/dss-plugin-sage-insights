e066a92caa33f8daad01b10ee345a2602c9b0fde
smazzei <Stephen.Mazzei@dataiku.com>
2025-09-04 14:07:50 +0000

    Edited file 'python-lib/sage/base_data/client_handle/instance/connections.py'

--- python-lib/sage/base_data/client_handle/instance/connections.py
+++ python-lib/sage/base_data/client_handle/instance/connections.py
@@ -1,7 +1,13 @@
 import pandas as pd
-from sage.src.dss_funcs import get_dss_name_id_mapping
 
 
 def main(client, client_d = {}):
-    #
+    connections = client.list_connections_names(connection_type="all")
+    dfs = []
+    for conn in connections:
+        conn_handle = client.get_connection(name=conn)
+        settings = conn_handle.get_settings()
+        d = settings.settings
+        dfs.append(pd.json_normalize(d))
+    df = pd.concat(dfs, ignore_index=True)
     return df
