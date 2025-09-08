65373f08329652579958a5ff881d7c24e5d5b21d
smazzei <Stephen.Mazzei@dataiku.com>
2025-09-08 16:32:22 +0000

    Edited file 'python-lib/sage/base_data/client_handle/projects/metadata.py'

--- python-lib/sage/base_data/client_handle/projects/metadata.py
+++ python-lib/sage/base_data/client_handle/projects/metadata.py
@@ -1,6 +1,8 @@
 import pandas as pd
-from sage.src.dss_funcs import get_nested_value
+import numpy as np
 from joblib import Parallel, delayed
+
+from sage.src.dss_funcs import get_nested_value
 
 
 def project_gather(projects):
