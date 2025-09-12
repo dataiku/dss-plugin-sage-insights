1e59c0f7746dec3f59aea8699309af9b1f512b1e
smazzei <Stephen.Mazzei@dataiku.com>
2025-09-12 18:50:38 +0000

    Edited file 'python-lib/sage/base_data/audit_log/event_mapping.py'

--- python-lib/sage/base_data/audit_log/event_mapping.py
+++ python-lib/sage/base_data/audit_log/event_mapping.py
@@ -38,7 +38,7 @@ def main(self, remote_client, df):
                 grp = grp.rename(columns={"projectKey": "project_key"})
             
             try:
-                write_path = f"/{instance_name}/dataiku_usage/{category}/{dt_year}/{dt_month}/{dt_day}/data-{dt_epoch}.csv"
+                write_path = f"/{instance_name}/dataiku_usage/{category}/{dt_year}/{dt_month}/{dt_day}/data-{dt_epoch}.parquet"
                 dss_folder.write_remote_folder_output(self, remote_client, write_path, grp)
                 results.append([f"write/save - Dataiku Usage {category}", True, f"data-{dt_epoch}.csv"])
             except Exception as e:
