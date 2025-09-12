14df589830c303784170781d034451efa86a6f0d
smazzei <Stephen.Mazzei@dataiku.com>
2025-09-12 15:21:24 +0000

    Edited file 'python-runnables/data-gather-audit-logs/runnable.py'

--- python-runnables/data-gather-audit-logs/runnable.py
+++ python-runnables/data-gather-audit-logs/runnable.py
@@ -60,6 +60,8 @@ class MyRunnable(Runnable):
         local_client = dss_funcs.build_local_client()
         instance_name = dss_funcs.get_dss_name(local_client)
         
+        #
+        from sage.src import dss_folder
         write_path = f"/testing.parquet"
         df = pd.json_normalize(local_client.list_projects())
         df["timestamp"] = datetime.now()
