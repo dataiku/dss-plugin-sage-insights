bbc0b7e0e4f93f6a205cf2635adb6e826f189966
smazzei <Stephen.Mazzei@dataiku.com>
2025-09-12 19:10:17 +0000

    Edited file 'python-runnables/data-gather-project/runnable.py'

--- python-runnables/data-gather-project/runnable.py
+++ python-runnables/data-gather-project/runnable.py
@@ -66,15 +66,16 @@ class MyRunnable(Runnable):
         
         # return results
         if results:
-            df = pd.DataFrame(results, columns=["project_key", "path", "module_name", "step", "result", "message"])
-            
-            
             # Save the df as a DS
-
+            df = pd.DataFrame(results, columns=["project_key", "path", "module_name", "step", "result", "message"])
+            project_handle = local_client.get_default_project()
+            dataset_handle = project_handle.get_dataset(dataset_name="dg_project_level")
+            if not dataset_handle.exists():
+                builder = project_handle.new_managed_dataset("dg_project_level")
+                builder.with_store_into("filesystem_managed")
+                dataset_handle = builder.create()
+            dataset = dataiku.Dataset("audit_log_cache")
             dataset.write_with_schema(df)
-            
-            
-            
             html = df.to_html()
             return html
         raise Exception("FAILED TO RUN PROJECT CHECKS")
