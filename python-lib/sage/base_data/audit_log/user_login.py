eb0cc1950553f5d4c3c60889d2883dc1f6a0e0f0
smazzei <Stephen.Mazzei@dataiku.com>
2025-09-12 15:32:31 +0000

    Edited file 'python-lib/sage/base_data/audit_log/user_login.py'

--- python-lib/sage/base_data/audit_log/user_login.py
+++ python-lib/sage/base_data/audit_log/user_login.py
@@ -4,17 +4,17 @@ from sage.src import dss_folder
 
 def main(self, remote_client, df):
     # Remove scenarios, job and NaN's
-    if "message.scenarioId" in df.columns:
-        df = df[df["message.scenarioId"].isna()]
-    if "message.jobId" in df.columns:
-        df = df[df["message.jobId"].isna()]
-    df = df[df["message.authSource"] == "USER_FROM_UI"]
-    df = df.dropna(subset=["message.authUser"])
+    if "message_scenarioId" in df.columns:
+        df = df[df["message_scenarioId"].isna()]
+    if "message_jobId" in df.columns:
+        df = df[df["message_jobId"].isna()]
+    df = df[df["message_authSource"] == "USER_FROM_UI"]
+    df = df.dropna(subset=["message_authUser"])
     df = df.dropna(axis=1, how='all')
 
     # Select the columns needed
     try:
-        df = df[["timestamp", "date", "message.callPath", "message.msgType", "message.authUser", "message.projectKey", "instance_name"]]
+        df = df[["timestamp", "date", "message_callPath", "message_msgType", "message_authUser", "message_projectKey", "instance_name"]]
     except:
         return ["Loading Audit Logs", False, "No new data found"]
 
@@ -30,7 +30,7 @@ def main(self, remote_client, df):
         dt_epoch = dt.value
 
         # Login Users
-        login_users = grp[grp["message.msgType"] == "application-open"]["message.authUser"].unique()
+        login_users = grp[grp["message_msgType"] == "application-open"]["message_authUser"].unique()
         login_users_df = pd.DataFrame(login_users, columns=["viewing_user_logins"])
         login_users_df["timestamp"] = pd.to_datetime(i)
         login_users_df["instance_name"] = instance_name
@@ -42,14 +42,14 @@ def main(self, remote_client, df):
             results.append(["write/save - All", False, e])
         
         # Developer Users
-        tdf = grp[grp["message.authUser"].isin(login_users)]
+        tdf = grp[grp["message_authUser"].isin(login_users)]
         action_words = ["save", "create", "analysis", "clear", "run"] # Action Words -- Focus on
         pattern = "|".join(action_words)
-        tdf = tdf[tdf["message.msgType"].str.contains(pattern, na=False)]
+        tdf = tdf[tdf["message_msgType"].str.contains(pattern, na=False)]
         remove_strings = ["list", "dataset-clear-samples", "dataset-save-schema", "project-save-variables"] # Vague Words -- Remove
         pattern = "|".join(remove_strings)
-        tdf = tdf[~tdf["message.msgType"].str.contains(pattern, na=False)]
-        developer_users = tdf["message.authUser"].unique()
+        tdf = tdf[~tdf["message_msgType"].str.contains(pattern, na=False)]
+        developer_users = tdf["message_authUser"].unique()
         developer_users_df = pd.DataFrame(developer_users, columns=["developer_user_logins"])
         developer_users_df["timestamp"] = pd.to_datetime(i)
         developer_users_df["instance_name"] = instance_name
