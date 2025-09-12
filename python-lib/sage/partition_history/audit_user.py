36530464c683d0b4a156b028cfeb9431b555d03e
smazzei <Stephen.Mazzei@dataiku.com>
2025-09-12 18:51:21 +0000

    Edited file 'python-lib/sage/partition_history/audit_user.py'

--- python-lib/sage/partition_history/audit_user.py
+++ python-lib/sage/partition_history/audit_user.py
@@ -15,7 +15,7 @@ def main(self, project_handle, folder, df):
                 sage_project_key = self.sage_project_key,
                 project_handle = project_handle,
                 folder_name = "base_data", 
-                path = f"/users/rolling_{module}.csv"
+                path = f"/users/rolling_{module}.parquet"
             )
             last_entry = pd.to_datetime(original_df["timestamp"].max())
         except:
