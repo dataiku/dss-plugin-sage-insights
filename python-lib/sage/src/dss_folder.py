0a4c6d6a4e6ee01e7904db21ef915a577b4ea1c4
smazzei <Stephen.Mazzei@dataiku.com>
2025-09-12 14:14:51 +0000

    Edited file 'python-lib/sage/src/dss_folder.py'

--- python-lib/sage/src/dss_folder.py
+++ python-lib/sage/src/dss_folder.py
@@ -23,7 +23,7 @@ def get_folder(sage_project_key, project_handle, folder_name):
 
 
 def create_folder(self, project_handle, folder_name):
-    SAGE_FOLDER_CONNECTION = os.environ["SAGE_FOLDER_CONNECTION"]
+    SAGE_FOLDER_CONNECTION = oself.SAGE_FOLDER_CONNECTION
     
     folder_handle = project_handle.create_managed_folder(
         name = folder_name,
