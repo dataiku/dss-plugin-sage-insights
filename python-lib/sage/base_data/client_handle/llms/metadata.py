eb9bdf0651fcad562320066669ae40e215553f5b
smazzei <Stephen.Mazzei@dataiku.com>
2025-09-12 15:54:35 +0000

    Edited file 'python-lib/sage/base_data/client_handle/llms/metadata.py'

--- python-lib/sage/base_data/client_handle/llms/metadata.py
+++ python-lib/sage/base_data/client_handle/llms/metadata.py
@@ -9,6 +9,5 @@ def main(self, client, client_d = {}):
     
     prefix = "llms"
     df = pd.json_normalize(project_handle.list_llms()).add_prefix(f"{prefix}_")
-    df = dss_funcs.rename_and_move_first(project_handle, df, f"{prefix}_projectKey", "project_key")
     df.drop(columns=["project_key"], inplace=True)
     return df
