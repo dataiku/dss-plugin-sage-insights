b4c9f28e7961c62daddc93886f23f13e7c4b53bd
smazzei <Stephen.Mazzei@dataiku.com>
2025-09-12 17:36:36 +0000

    Edited file 'python-lib/sage/base_data/project_handle/datasets/metadata.py'

--- python-lib/sage/base_data/project_handle/datasets/metadata.py
+++ python-lib/sage/base_data/project_handle/datasets/metadata.py
@@ -6,12 +6,14 @@ def main(self, project_handle, client_d = {}):
     if not project_handle.list_datasets():
         return pd.DataFrame()
     
-    df = pd.json_normalize(project_handle.list_datasets()).add_prefix("dataset_")
-    
+    prefix = "dataset_"
+    df = pd.json_normalize(project_handle.list_datasets()).add_prefix(prefix)
     # Clean dates
     for c in ["dataset_versionTag.lastModifiedOn", "dataset_creationTag.lastModifiedOn"]:
         df[c] = pd.to_datetime(df[c], unit="ms", utc=True)
         df[c] = df[c].fillna(pd.to_datetime("1970-01-01", utc=True))
         df[c] = df[c].dt.strftime("%Y-%m-%d %H:%M:%S.%f")
+    # Project Key
+    df = rename_and_move_first(project_handle, df, f"{prefix}projectKey", "project_key")
         
     return df
