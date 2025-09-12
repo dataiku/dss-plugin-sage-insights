0b8a41c27471a56ddf4fa58905c89ca614a948a1
smazzei <Stephen.Mazzei@dataiku.com>
2025-09-12 16:39:54 +0000

    Edited file 'python-lib/sage/base_data/client_handle/projects/metadata.py'

--- python-lib/sage/base_data/client_handle/projects/metadata.py
+++ python-lib/sage/base_data/client_handle/projects/metadata.py
@@ -27,4 +27,10 @@ def main(self, client, client_d = {}):
     df.columns = df.columns.str.replace(r"^project_project", "project_", regex=True).str.lower()
     df = df.rename(columns={"project_ownerlogin": "login"})
     
+    # Convert Dict to strings
+    for col in df.columns:
+        types = df[col].dropna().map(type).unique()
+        if any(t in (dict, list) for t in types):
+            df[col] = df[col].astype(str)
+    
     return df
