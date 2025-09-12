fbf812fa67b5dd80c58ac811092b4601e8b95f96
smazzei <Stephen.Mazzei@dataiku.com>
2025-09-12 16:09:20 +0000

    Edited file 'python-lib/sage/base_data/client_handle/projects/metadata.py'

--- python-lib/sage/base_data/client_handle/projects/metadata.py
+++ python-lib/sage/base_data/client_handle/projects/metadata.py
@@ -13,7 +13,7 @@ def project_gather(projects):
         d["login"] = project.get("ownerLogin", "")
         d["project_login_dn"] = get_nested_value(project, ["ownerDisplayName"])
         d["project_last_mod_by"] = get_nested_value(project, ["versionTag", "lastModifiedBy", "login"])
-        d["project_last_mod_dt"] = get_nested_value(project, ["versionTag", "lastModifiedOn"])
+        d["project_last_mod_dt"] = get_nested_value(project, ["versionTag", "lastModifiedOn"], dt=True)
         d["project_last_create_by"] = get_nested_value(project, ["creationTag", "lastModifiedBy", "login"])
         d["project_last_create_dt"] = get_nested_value(project, ["creationTag", "lastModifiedOn"])
         d["project_shortDesc"] = project.get("shortDesc", "")
