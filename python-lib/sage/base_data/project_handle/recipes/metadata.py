a25952b2fedbb22d549a2e48dcf13972385fc1d6
smazzei <Stephen.Mazzei@dataiku.com>
2025-09-12 21:04:41 +0000

    Edited file 'python-lib/sage/base_data/project_handle/recipes/metadata.py'

--- python-lib/sage/base_data/project_handle/recipes/metadata.py
+++ python-lib/sage/base_data/project_handle/recipes/metadata.py
@@ -60,20 +60,19 @@ def main(self, project_handle, client_d = {}):
         recipes_type = getattr(row, "recipes_type")
         recipe_handle = project_handle.get_recipe(recipes_name)
         # Recipe Engine Better Details
-        if recipes_type in ["python", "R"]:
-            try:
-                recipe_engine_type = recipe_handle.get_status().get_selected_engine_details()["type"]
-                recipe_engine_label = recipe_handle.get_status().get_selected_engine_details()["label"]
-                recipe_engine_recommended = recipe_handle.get_status().get_selected_engine_details()["recommended"]
-            except:
-                recipe_engine_type = "NOT_FOUND"
-                recipe_engine_label = "NOT_FOUND"
-                recipe_engine_recommended = "NOT_FOUND"
-            df.loc[df["recipes_name"] == recipes_name, "recipes_params_engineType"] = recipe_engine_type
-            df.loc[df["recipes_name"] == recipes_name, "recipes_params_engineLabel"] = recipe_engine_label
-            df.loc[df["recipes_name"] == recipes_name, "recipes_params_engineRecommended"] = recipe_engine_recommended
-            if getattr(row, "recipes_params_containerSelection_containerMode") == "INHERIT":
-                df.loc[df["recipes_name"] == recipes_name, "recipes_params_containerSelection_containerConf"] = container_env_name
+        try:
+            recipe_engine_type = recipe_handle.get_status().get_selected_engine_details()["type"]
+            recipe_engine_label = recipe_handle.get_status().get_selected_engine_details()["label"]
+            recipe_engine_recommended = recipe_handle.get_status().get_selected_engine_details()["recommended"]
+        except:
+            recipe_engine_type = "NOT_FOUND"
+            recipe_engine_label = "NOT_FOUND"
+            recipe_engine_recommended = "NOT_FOUND"
+        df.loc[df["recipes_name"] == recipes_name, "recipes_params_engineType"] = recipe_engine_type
+        df.loc[df["recipes_name"] == recipes_name, "recipes_params_engineLabel"] = recipe_engine_label
+        df.loc[df["recipes_name"] == recipes_name, "recipes_params_engineRecommended"] = recipe_engine_recommended
+        if getattr(row, "recipes_params_containerSelection_containerMode", "") == "INHERIT":
+            df.loc[df["recipes_name"] == recipes_name, "recipes_params_containerSelection_containerConf"] = container_env_name
         
         # Individual Objects
         if recipes_type == "python":
