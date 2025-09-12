c9f422e9dd8d62bfcd6f5df07807ca5e0001fcd6
smazzei <Stephen.Mazzei@dataiku.com>
2025-09-12 18:53:37 +0000

    Edited file 'python-runnables/data-gather-diskspace/runnable.py'

--- python-runnables/data-gather-diskspace/runnable.py
+++ python-runnables/data-gather-diskspace/runnable.py
@@ -102,7 +102,7 @@ class MyRunnable(Runnable):
         dt_day   = str(f'{self.dt.day:02d}')
         df["instance_name"] = instance_name
         try:
-            write_path = f"/{instance_name}/disk_space/diskspace/{dt_year}/{dt_month}/{dt_day}/data.csv"
+            write_path = f"/{instance_name}/disk_space/diskspace/{dt_year}/{dt_month}/{dt_day}/data.parquet"
             dss_folder.write_remote_folder_output(self, remote_client, write_path, df)
             results.append(["write/save", True, None])
         except Exception as e:
