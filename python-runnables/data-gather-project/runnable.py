c6ca79ba10a95d18cb2417e65448dc50c53d6631
smazzei <Stephen.Mazzei@dataiku.com>
2025-09-12 19:26:04 +0000

    Edited file 'python-runnables/data-gather-project/runnable.py'

--- python-runnables/data-gather-project/runnable.py
+++ python-runnables/data-gather-project/runnable.py
@@ -73,6 +73,6 @@ class MyRunnable(Runnable):
             for col in ["project_key", "path", "module_name", "step", "result", "message"]:
                 rt.add_column(n, col, "STRING")
                 n +=1
-            rt.add_record(results)
+            #.add_record(results)
             return rt
         raise Exception("FAILED TO RUN PROJECT CHECKS")
