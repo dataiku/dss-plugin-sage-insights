7d5740c4b7fac7b53a210394276532c5e516efd6
smazzei <Stephen.Mazzei@dataiku.com>
2025-09-12 14:57:29 +0000

    Edited file 'streamlit/src/dss_streamlit.py'

--- streamlit/src/dss_streamlit.py
+++ streamlit/src/dss_streamlit.py
@@ -1,9 +1,3 @@
-
-
-
-import sys
-sys.dont_write_bytecode = True
-
 import streamlit as st
 import pandas as pd
 from pandas.api.types import ( 
@@ -17,6 +11,52 @@ from pandas.api.types import (
 from sage.src import dss_funcs
 from sage.src import dss_folder
 
+
+
+
+# ---------- STREAMLIT MODULES -----------------------------
+def collect_modules(module):
+    import streamlit as st
+    d = {}
+    directory = module.__path__[0]
+    for root, _, files in os.walk(directory):
+        for f in files:
+            if f.endswith(".py") and f != "__init__.py":
+                module_name = f[:-3]
+                path = root.replace(directory, "")
+                fp = os.path.join(root, f)
+                delimiters = r'[-_]'
+                words = re.split(delimiters, module_name)
+                capitalized_words = [word.capitalize() for word in words]
+                final_string = " ".join(capitalized_words)
+                d[final_string] = [module_name, fp]
+    return d
+
+
+def collect_display_data(load_modules):
+    display_data = []
+    modules = collect_modules(load_modules)
+    for key in modules.keys():
+        r_type = key.split(" ")
+        r_type = r_type[0].lower()
+        display_data.append(key)
+    return modules, display_data
+
+
+def load_insights(module_name, fp, filters = {}):
+    results = {}
+    spec = importlib.util.spec_from_file_location(module_name, fp)
+    try:
+        module = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(module)
+        if hasattr(module, 'main'):
+            results = module.main(filters)
+    except Exception as e:
+        import streamlit as st
+        st.error(f"Error importing or running ({fp}) {module_name}: {e}")
+        results = {}
+        return results
+    return results
 
 # ------------------------------------------------------------------------------------
 def get_datasets(data_category):
