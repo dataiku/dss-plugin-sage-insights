0d5a6b2dd41ca0320bbe59162b1716ac904084b6
smazzei <Stephen.Mazzei@dataiku.com>
2025-09-12 17:27:04 +0000

    Edited file 'python-lib/sage/base_data/project_handle/datasets/metadata.py'

--- python-lib/sage/base_data/project_handle/datasets/metadata.py
+++ python-lib/sage/base_data/project_handle/datasets/metadata.py
@@ -13,4 +13,5 @@ def main(self, project_handle, client_d = {}):
         df[c] = pd.to_datetime(df[c], unit="ms", utc=True)
         df[c] = df[c].fillna(pd.to_datetime("1970-01-01", utc=True))
         df[c] = df[c].dt.strftime("%Y-%m-%d %H:%M:%S.%f")
+        
     return df
