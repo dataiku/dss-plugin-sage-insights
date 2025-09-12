c743c250a52be1a51758235bced905183ced629f
smazzei <Stephen.Mazzei@dataiku.com>
2025-09-12 19:46:34 +0000

    Edited file 'python-runnables/data-gather-partition-history/runnable.py'

--- python-runnables/data-gather-partition-history/runnable.py
+++ python-runnables/data-gather-partition-history/runnable.py
@@ -70,3 +70,5 @@ class MyRunnable(Runnable):
             for index, row in df.iterrows():
                 rt.add_record(row.tolist())
             return rt
+        else:
+            raise Exception("Something went wrong")
