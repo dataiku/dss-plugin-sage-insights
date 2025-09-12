63f113bd2618788a5b829358251332f7a32aff4b
smazzei <Stephen.Mazzei@dataiku.com>
2025-09-12 18:44:34 +0000

    Edited file 'python-runnables/initialize-dashboard/runnable.py'

--- python-runnables/initialize-dashboard/runnable.py
+++ python-runnables/initialize-dashboard/runnable.py
@@ -18,7 +18,6 @@ class MyRunnable(Runnable):
 
         # Set environment variable
         self.sage_folder_connection = plugin_config.get("sage_folder_connection", "filesystem_folders")
-        os.environ["SAGE_FOLDER_CONNECTION"] = self.sage_folder_connection
         
     def get_progress_target(self):
         return None
