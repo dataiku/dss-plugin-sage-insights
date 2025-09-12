50b4b7446c87d52d5fca1206fd19706774b58f43
smazzei <Stephen.Mazzei@dataiku.com>
2025-09-12 16:27:36 +0000

    Edited file 'python-lib/sage/base_data/client_handle/projects/metadata.py'

--- python-lib/sage/base_data/client_handle/projects/metadata.py
+++ python-lib/sage/base_data/client_handle/projects/metadata.py
@@ -15,7 +15,7 @@ def main(self, client, client_d = {}):
     # Rename a few colums
     df.columns = df.columns.str.replace(r"^project_project", "project_", regex=True)
     df = df.rename(columns={"project_Key": "project_key"})
-    df = df.rename(columns={"project_projectKey": "login"})
+    df = df.rename(columns={"project_ownerLogin": "login"})
 
     # Imported projects missing creation values - temp fix for now
     df.loc[df["project_creationTag_versionNumber"].isna(), "project_creationTag_versionNumber"] = 0
