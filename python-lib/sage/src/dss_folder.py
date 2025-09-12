b28ebcc46cbea254a4c4e2f444f6ff7a46fe2423
smazzei <Stephen.Mazzei@dataiku.com>
2025-09-12 14:34:38 +0000

    Edited file 'python-lib/sage/src/dss_folder.py'

--- python-lib/sage/src/dss_folder.py
+++ python-lib/sage/src/dss_folder.py
@@ -56,11 +56,9 @@ def function_with_warning(df):
 def read_local_folder_input(self, project_handle, folder_name, path, data_type="DF"):
     folder = get_folder(self, project_handle, folder_name)
     if data_type == "DF":
-        with folder.get_download_stream(path) as reader:
-            data = pd.read_csv(reader)
-            #with warnings.catch_warnings():
-                #warnings.simplefilter("ignore")  # Ignore all warnings within this block
-                #data = function_with_warning(data)
+        with folder.get_download_stream("testing.parquet") as stream:
+            file_bytes = io.BytesIO(stream.read())
+            data = pd.read_parquet(file_bytes)
     elif data_type == "JSON":
         with folder.get_download_stream(path) as reader:
             data = json.loads(reader.data)
