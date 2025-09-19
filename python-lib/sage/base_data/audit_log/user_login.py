78884c2c5b05bc16fc4ae08644c9ef224cde56aa
smazzei <Stephen.Mazzei@dataiku.com>
2025-09-19 19:56:34 +0000

    Edited file 'python-lib/sage/base_data/audit_log/user_login.py'

--- python-lib/sage/base_data/audit_log/user_login.py
+++ python-lib/sage/base_data/audit_log/user_login.py
@@ -11,6 +11,8 @@ def main(self, remote_client, df):
     df = df[df["message_authSource"] == "USER_FROM_UI"]
     df = df.dropna(subset=["message_login"])
     df = df.dropna(axis=1, how='all')
+    
+    return
 
     # Select the columns needed
     try:
