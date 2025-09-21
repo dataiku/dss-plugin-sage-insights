2b22a6dc5b467fe5bb38218e0fe1af5f2e5f505b
smazzei <Stephen.Mazzei@dataiku.com>
2025-09-21 18:54:02 +0000

    Edited file 'python-lib/sage/base_data/project_handle/datasets/metadata.py'

--- python-lib/sage/base_data/project_handle/datasets/metadata.py
+++ python-lib/sage/base_data/project_handle/datasets/metadata.py
@@ -14,7 +14,6 @@ def main(self, project_handle, client_d = {}):
             continue
         df[c] = pd.to_datetime(df[c], unit="ms", utc=True)
         df[c] = df[c].fillna(pd.to_datetime("1970-01-01", utc=True))
-        df[c] = df[c].dt.strftime("%Y-%m-%d %H:%M:%S.%f")
     # Project Key
     df = dss_funcs.rename_and_move_first(project_handle, df, f"{prefix}projectKey", "project_key")
         
